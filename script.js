7552fd813b81b2747182bb95f7f46c3bf8556625
Anaïs <aekyon.08@gmail.com>
2023-11-14 12:15:33 +0100

    Add flexbox structure

--- script.js
+++ script.js
@@ -1,55 +1,56 @@
+// Weather Book
+import weatherBook from './weatherCode.js';
+
 //Display city information + forecast
-async function searchCity () {
-    let inputCity = document.querySelector("input").value;
-    let chosenCity = await fetch ("https://geocoding-api.open-meteo.com/v1/search?name=" + encodeURIComponent(inputCity));
-    let cityInformation = await chosenCity.json();
-    //Name
-    let cityName = document.createElement("h2");
-    document.body.append(cityName);
-    cityName.textContent = cityInformation.results[0].name;
-    //Country
-    let cityCountry = document.createElement("h3");
-    document.body.append(cityCountry);
-    cityCountry.textContent = cityInformation.results[0].country;
-    //Lattitude
-    let cityLatitude = document.createElement("h4");
-    document.body.append(cityLatitude);
-    cityLatitude.textContent = cityInformation.results[0].latitude;
-    //Longitude
-    let cityLongitude = document.createElement("h4");
-    document.body.append(cityLongitude);
-    cityLongitude.textContent = cityInformation.results[0].longitude;
+async function getWeather () {
+    const inputCity = document.querySelector("input").value;
+    const geoApi = "https://geocoding-api.open-meteo.com/v1/search?name=" + encodeURIComponent(inputCity);
+    const getCity = await fetch (geoApi);
+    const cityInformation = await getCity.json();
     //All info
     //console.log(JSON.stringify(cityInformation, null, 2));
-
+    //Name
+    const main = document.querySelector("main");
+    const cityName = document.createElement("h2");
+    main.append(cityName);
+    cityName.textContent = cityInformation.results[0].name;
+    //Country
+    const cityCountry = document.createElement("h3");
+    main.append(cityCountry);
+    cityCountry.textContent = cityInformation.results[0].country;
+    //Lattitude
+    const cityLatitude = cityInformation.results[0].latitude;
+    //Longitude
+    const cityLongitude = cityInformation.results[0].longitude;
     //Forecast
-    let cityForecast = await fetch("https://api.open-meteo.com/v1/forecast?latitude=" + encodeURIComponent(cityLatitude.textContent) + "&longitude=" + encodeURIComponent(cityLongitude.textContent) + "&hourly=temperature_2m&daily=weather_code");
-    
+    const weatherApi = await fetch("https://api.open-meteo.com/v1/forecast?latitude=" + encodeURIComponent(cityLatitude) + "&longitude=" + encodeURIComponent(cityLongitude) + "&hourly=temperature_2m&daily=weather_code");
     //https://api.open-meteo.com/v1/forecast?latitude=48.2085&longitude=16.3721&hourly=temperature_2m&daily=weather_code
-    let forecastInformation = await cityForecast.json();
-    console.log(forecastInformation);
-
-    //let todayForecast =
-
+    const forecastInformation = await weatherApi.json();
+    //console.log(forecastInformation);
+    //const weekDays = forecastInformation.daily.time;
+    const weathersCodes = forecastInformation.daily.weather_code;
+    weathersCodes.forEach(code => {
+        if (weatherBook.hasOwnProperty(code)) {
+            const weathersDescription = document.createElement("section");
+            weathersDescription.classList.add('forecast-section');
+            weathersDescription.textContent = weatherBook[code].day.description;
+            main.append(weathersDescription);
+        } else {
+            const codeNotFound = document.createElement("section");
+            codeNotFound.textContent = "Weather Code not found, but if it's Belgium it'll probably rain."
+            main.append(codeNotFound);
+        }
+    });
 }
 
 const searchButton = document.querySelector("button");
 searchButton.addEventListener("click", () => {
-    searchCity();
+    getWeather();
 })
 
-/*PRESSENTER FUNCTION pressEnter.addEventListener("keyup", (event) => {
-    if (event.isComposing || event.keyCode === 13) {
-        //code to execute
+const inputField = document.querySelector("input");
+inputField.addEventListener("keyup", (event) => {
+    if (event.code === 'Enter') {
+        getWeather();
     }
-})*/
-
-
-
-
-//TODO: On clicking the SUBMIT button or pressing ENTER:
-        //Use an api to define the city geo-location data from the user-input
-        //Use an api to get the weather data for at least the next 5 days
-        //Manipulate your DOM in order to display the weather for the next 5 days in your application.
-//TODO: Find a way to make those API calls asynchronous.
-//TODO: The application must be responsive, accessible and mobile friendly
+})
