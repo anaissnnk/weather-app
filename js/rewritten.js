59e9d02c99af93a31e4f8fa2d50a86143aa3a9dd
Anaïs <aekyon.08@gmail.com>
2023-11-21 17:02:02 +0100

    Change function to modern syntax

--- js/rewritten.js
+++ js/rewritten.js
@@ -1,17 +1,40 @@
 // Weather Book
 //import weatherBook from './weatherCode.js';
 
+'use strict'
+
 //get city
-async function getCityInformation () {
+
+const getCityInformation = async () => {
     const inputCity = document.getElementById('city').value;
     const geoApi = await fetch("https://geocoding-api.open-meteo.com/v1/search?name=" + encodeURIComponent(inputCity));
     const cityInformation = await geoApi.json();
     console.dir(cityInformation);
+
+    //Lattitude
+    const cityLatitude = cityInformation.results[0].latitude;
+    console.log(cityLatitude)
+    //Longitude
+    const cityLongitude = cityInformation.results[0].longitude;
+    console.log(cityLongitude);
+
+    return { cityInformation, cityLatitude, cityLongitude };
 }
 
-//get geoLocalization
-async function getWeather () {
-    //code to execute;
+const getWeather = async() => {
+    try {
+        // Call getCityInformation() and extract latitude, and longitude
+        const { cityLatitude, cityLongitude } = await getCityInformation();
+
+        const weatherApi = await fetch("https://api.open-meteo.com/v1/forecast?latitude=" + encodeURIComponent(cityLatitude) + "&longitude=" + encodeURIComponent(cityLongitude) + "&hourly=temperature_2m&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=Europe%2FBerlin");
+        const forecastInformation = await weatherApi.json();
+        console.log(forecastInformation);
+
+        return forecastInformation;
+
+    } catch (error) {
+        console.error("Error fetching weather information:", error);
+    }
 }
 
 
@@ -30,16 +53,17 @@ const inputField = document.querySelector("input");
 inputField.addEventListener("keyup", (event) => {
     if (event.code === 'Enter') {
         getCityInformation();
+        getWeather();
     }
 })
 
 //NEW SEARCH
-function newSearch() {
+const newSearch = () => {
     //code to execute
 }
 
 //DATE FUNCTION
-function getCurrentDate() {
+const getCurrentDate = () => {
     const currentDate = new Date();
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     return currentDate.toLocaleDateString('en-US', options);
