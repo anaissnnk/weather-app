44cac30be2517b355704ef59e646520d56d59fde
Anaïs <aekyon.08@gmail.com>
2023-11-21 17:06:43 +0100

    Refractor all functions

--- js/rewritten.js
+++ js/rewritten.js
@@ -1,10 +1,9 @@
 // Weather Book
-//import weatherBook from './weatherCode.js';
+import weatherBook from './weatherCode.js';
 
 'use strict'
 
 //get city
-
 const getCityInformation = async () => {
     const inputCity = document.getElementById('city').value;
     const geoApi = await fetch("https://geocoding-api.open-meteo.com/v1/search?name=" + encodeURIComponent(inputCity));
@@ -21,6 +20,7 @@ const getCityInformation = async () => {
     return { cityInformation, cityLatitude, cityLongitude };
 }
 
+//get weather
 const getWeather = async() => {
     try {
         // Call getCityInformation() and extract latitude, and longitude
@@ -37,15 +37,65 @@ const getWeather = async() => {
     }
 }
 
+//createCard
 
+const createCard = async () => {
+    let container;
+    for (let i = 0; i < 5; i++) {
+        if (i <=2 ) {
+            container = document.getElementById("forecast-firstsection");
+        } else {
+            container = document.getElementById("forecast-secondsection");
+        }
+    
+    const weatherArticle = document.createElement("article");
+    weatherArticle.classList.add("forecast-article");
+    container.append(weatherArticle);
 
+    const { cityLatitude, cityLongitude } = await getCityInformation();
+    
+    //add date
+    const weatherApi = await fetch("https://api.open-meteo.com/v1/forecast?latitude=" + encodeURIComponent(cityLatitude) + "&longitude=" + encodeURIComponent(cityLongitude) + "&hourly=temperature_2m&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=Europe%2FBerlin");
+    const forecastInformation = await weatherApi.json();
+    const articleDate = document.createElement("h2");
+    articleDate.classList.add("article-date");
+    articleDate.textContent = forecastInformation.daily.time[i];
+    weatherArticle.append(articleDate);
+
+    //add weather code description
+    const weatherHeading = document.createElement("h3");
+    weatherHeading.classList.add("weather-description");
+    weatherHeading.textContent = weatherBook[forecastInformation.daily.weather_code[i]].day.description;
+    weatherArticle.append(weatherHeading);
+
+    //add weather img
+    const weatherImg = document.createElement("img");
+    weatherImg.classList.add("weather-img");
+    weatherImg.src = weatherBook[forecastInformation.daily.weather_code[i]].day.image;
+    weatherArticle.append(weatherImg);
+
+    //add min temperature
+    const minTemperature = document.createElement("span");
+    minTemperature.classList.add('min-temperature');
+    minTemperature.textContent = "Min temperature: " + forecastInformation.daily.temperature_2m_min[i];
+    weatherArticle.append(minTemperature);
+
+    //add max temperature
+    const maxTemperature = document.createElement("span");
+    maxTemperature.classList.add('max-temperature');
+    maxTemperature.textContent = "Max temperature: " + forecastInformation.daily.temperature_2m_max[i];
+    weatherArticle.append(maxTemperature);  
+}
+}
 
 
 
 //SEARCH BUTTON
 const searchButton = document.querySelector("button");
 searchButton.addEventListener("click", () => {
-    //code to execute
+    getCityInformation();
+    getWeather();
+    createCard();
 })
 
 //PRESS ENTER
@@ -54,6 +104,7 @@ inputField.addEventListener("keyup", (event) => {
     if (event.code === 'Enter') {
         getCityInformation();
         getWeather();
+        createCard();
     }
 })
 
@@ -75,4 +126,3 @@ const currentDate = document.createElement("span");
 currentDate.classList.add("current-date");
 currentDate.textContent = "Today is " + getCurrentDate();
 dateSection.append(currentDate);
-
